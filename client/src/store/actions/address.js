ff74da2a6f9a07b319e450ae4fbdef2bfdbc0fca
Karen Posada <k100posada@gmail.com>
2018-09-23 21:53:42 -0500

    fetch transactions fix

--- client/src/store/actions/address.js
+++ client/src/store/actions/address.js
@@ -1,19 +1,6 @@
 import axios from "axios";
 import { FETCH_TRANSACTIONS } from "./constants";
 
-// export function fetchTransactions(id) {
-//   return async function(dispatch) {
-//     const res = await fetch(`/api/address/${id}`);
-//     return dispatch({
-//       type: FETCH_TRANSACTIONS,
-//       payload: {
-//         id,
-//         ...res
-//       }
-//     });
-//   };
-// }
-
 export const fetchTransactions = id => dispatch => {
   axios
     .get(`http://localhost:5000/api/address/${id}`)
@@ -25,15 +12,3 @@ export const fetchTransactions = id => dispatch => {
     )
     .catch(err => console.error(err));
 };
-// export const fetchApi = id => async dispatch => {
-//   const res = await axios.get(
-//     `http://api.etherscan.io/api?module=account&action=txlist&address=${id}&startblock=0&endblock=99999999&sort=asc&apikey=AH56YE6FZWX7QHMR6JFV3FGHCNWCXCVKCV`
-//   );
-//   dispatch({
-//     type: FETCH_TRANSACTIONS,
-//     payload: {
-//       id,
-//       ...res.data
-//     }
-//   });
-// };
